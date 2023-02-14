b9aa6652cdff4a99bb63267a2a29d63e29492270
VickerTan <vickerpangestan@gmail.com>
2023-02-14 23:22:02 +0800

    Add edit functionality and new object property

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const db = new Dexie("Shopping App");
-db.version(1).stores({ items: '++id,name,quantity,purchased' });
+db.version(1).stores({ items: '++id,name,quantity,purchased,readonly' });
 
 const itemForm = document.querySelector("#item-form");
 const itemList = document.querySelector("#item-list");
@@ -14,18 +14,36 @@ const createItemDiv = async () => {
             <input
                 type="checkbox"
                 id="checkbox"
-                onchange="toggleItemStatus(event, ${item.id})"
+                onchange="toggleStatus(event, ${item.id})"
                 ${item.purchased && 'checked'}
                 />
             <span class="bubble"></span>
         </label>
         <div class="content ${item.purchased && 'purchased'}">
-            <p class="name-content">${item.name}</p>
-            <p class="price-content">$${item.price}</p>
-            <p class="quantity-content">x ${item.quantity}</p>
+            <input
+                type="text"
+                class="name-content"
+                id="name-content"
+                value="${item.name}"
+                ${item.readonly && 'readonly'}
+                />
+            <input
+                type="text"
+                class="price-content"
+                id="price-content"
+                value="$${item.price}"
+                ${item.readonly && 'readonly'}
+                />
+            <input
+                type="text"
+                class="quantity-content"
+                id="quantity-content"
+                value="x ${item.quantity}"
+                ${item.readonly && 'readonly'}
+                />
         </div>
         <div class="actions">
-            <button class="edit">EDIT</button>
+            <button class="edit" onclick="toggleEdit()">EDIT</button>
             <button class="delete" onclick="removeItem(${item.id})">X</button>
         </div>
     </div>
@@ -57,13 +75,16 @@ const removeAllItem = async () => {
     await createItemDiv();
 }
 
+const toggleEdit = async (id) => {
+    await db.items.update(id, { readonly: false });
+}
+
 const removeItem = async (id) => {
     await db.items.delete(id);
     await createItemDiv();
 }
 
-const toggleItemStatus = async (event, id) => {
+const toggleStatus = async (event, id) => {
     await db.items.update(id, { purchased: event.target.checked });
     await createItemDiv();
 }
-
