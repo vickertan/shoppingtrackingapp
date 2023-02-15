4d65b4b40acace283f76ecb025d5b759753c389b
VickerTan <vickerpangestan@gmail.com>
2023-02-15 23:17:59 +0800

    Switch readonly and change button text on click

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const db = new Dexie("Shopping App");
-db.version(1).stores({ items: '++id,name,quantity,purchased,readonly' });
+db.version(1).stores({ items: '++id' });
 
 const itemForm = document.querySelector("#item-form");
 const itemList = document.querySelector("#item-list");
@@ -25,25 +25,25 @@ const createItemDiv = async () => {
                 class="name-content"
                 id="name-content"
                 value="${item.name}"
-                ${item.readonly && 'readonly'}
+                ${!item.readonly && 'readonly'}
                 />
             <input
                 type="text"
                 class="price-content"
                 id="price-content"
                 value="$${item.price}"
-                ${item.readonly && 'readonly'}
+                ${!item.readonly && 'readonly'}
                 />
             <input
                 type="text"
                 class="quantity-content"
                 id="quantity-content"
                 value="x ${item.quantity}"
-                ${item.readonly && 'readonly'}
+                ${!item.readonly && 'readonly'}
                 />
         </div>
         <div class="actions">
-            <button class="edit" onclick="toggleEdit()">EDIT</button>
+            <button class="edit" onclick="toggleEdit(event, ${item.id})">${item.buttonText}</button>
             <button class="delete" onclick="removeItem(${item.id})">X</button>
         </div>
     </div>
@@ -64,7 +64,14 @@ itemForm.onsubmit = async (e) => {
     const priceInput = document.querySelector('#item-price').value;
     const quantityInput = document.querySelector('#item-quantity').value;
 
-    await db.items.add({ name: nameInput , quantity: quantityInput, price: priceInput });
+    await db.items.add({
+        name: nameInput,
+        quantity: quantityInput,
+        price: priceInput,
+        buttonText: "EDIT",
+        nameEdit: ""
+    });
+
     await createItemDiv();
 
     itemForm.reset();
@@ -75,8 +82,21 @@ const removeAllItem = async () => {
     await createItemDiv();
 }
 
-const toggleEdit = async (id) => {
-    await db.items.update(id, { readonly: false });
+const toggleEdit = (event, id) => {
+    const nameContent = event.target.parentElement.parentElement.querySelector('.content').querySelector('.name-content').value;
+
+    if (event.target.innerHTML == "EDIT") {
+        db.items.update(id, { buttonText: "SAVE" });
+        db.items.update(id, { readonly: true });
+        // db.items.update(id, { nameEdit: })
+        console.log(nameContent);
+        createItemDiv();
+    } else {
+        db.items.update(id, { buttonText: "EDIT" });
+        db.items.update(id, { readonly: false });
+        console.log(nameContent);
+        createItemDiv();
+    }
 }
 
 const removeItem = async (id) => {
